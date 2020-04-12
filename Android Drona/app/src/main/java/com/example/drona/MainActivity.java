eec090f2acd57aa8b02f7eca6e72ed23c39b15cd
fang009 <surya91521@gmail.com>
2020-04-13 00:13:30 +0530

    Google id going into mongo atlas

--- Android Drona/app/src/main/java/com/example/drona/MainActivity.java
+++ Android Drona/app/src/main/java/com/example/drona/MainActivity.java
@@ -14,7 +14,23 @@ import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.mongodb.lang.NonNull;
+import com.mongodb.stitch.android.core.Stitch;
+import com.mongodb.stitch.android.core.StitchAppClient;
+import com.mongodb.stitch.android.core.auth.StitchUser;
+import com.mongodb.stitch.android.services.mongodb.remote.RemoteMongoClient;
+import com.mongodb.stitch.android.services.mongodb.remote.RemoteMongoCollection;
+import com.mongodb.stitch.core.auth.providers.anonymous.AnonymousCredential;
+import com.mongodb.stitch.core.services.mongodb.remote.RemoteUpdateOptions;
+import com.mongodb.stitch.core.services.mongodb.remote.RemoteUpdateResult;
+
+import org.bson.Document;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -51,6 +67,8 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+
 
 
     }
@@ -97,6 +115,68 @@ public class MainActivity extends AppCompatActivity {
         try {
             GoogleSignInAccount account = completedTask.getResult(ApiException.class);
             personId = account.getId();
+
+
+            final StitchAppClient client =
+                    Stitch.initializeDefaultAppClient("drona-mnbxm");
+
+            final RemoteMongoClient mongoClient =
+                    client.getServiceClient(RemoteMongoClient.factory, "mongodb-atlas");
+
+            final RemoteMongoCollection<Document> coll =
+                    mongoClient.getDatabase("test").getCollection("user");
+
+
+
+            client.getAuth().loginWithCredential(new AnonymousCredential()).continueWithTask(
+                    new Continuation<StitchUser, Task<RemoteUpdateResult>>() {
+
+                        @Override
+                        public Task<RemoteUpdateResult> then(@NonNull Task<StitchUser> task) throws Exception {
+                            if (!task.isSuccessful()) {
+                                Log.e("STITCH", "Login failed!");
+                                throw task.getException();
+                            }
+
+                            final Document updateDoc = new Document(
+                                    "owner_id",
+                                    task.getResult().getId()
+                            );
+
+                            updateDoc.put("Google  ID", personId);
+                            return coll.updateOne(
+                                    null, updateDoc, new RemoteUpdateOptions().upsert(true)
+                            );
+                        }
+                    }
+            ).continueWithTask(new Continuation<RemoteUpdateResult, Task<List<Document>>>() {
+                @Override
+                public Task<List<Document>> then(@NonNull Task<RemoteUpdateResult> task) throws Exception {
+                    if (!task.isSuccessful()) {
+                        Log.e("STITCH", "Update failed!");
+                        throw task.getException();
+                    }
+                    List<Document> docs = new ArrayList<>();
+                    return coll
+                            .find(new Document("owner_id", client.getAuth().getUser().getId()))
+                            .limit(100)
+                            .into(docs);
+                }
+            }).addOnCompleteListener(new OnCompleteListener<List<Document>>() {
+                @Override
+                public void onComplete(@NonNull Task<List<Document>> task) {
+                    if (task.isSuccessful()) {
+                        Log.d("STITCH", "Found docs: " + task.getResult().toString());
+                        return;
+                    }
+                    Log.e("STITCH", "Error: " + task.getException().toString());
+                    task.getException().printStackTrace();
+                }
+            });
+
+
+
+
             Intent intent = new Intent(MainActivity.this,User.class);
             intent.putExtra("ID",personId);
             startActivity(intent);
